08f646b3cfaf0f6b704760a49de95fecb74c08da
dek <dakoner@gmail.com>
2022-01-30 21:38:12 +0000

    make target parameterized

--- serial_interface.py
+++ serial_interface.py
@@ -5,6 +5,8 @@ import threading
 import queue
 import paho.mqtt.client as mqtt
 
+MQTT_SERVER="gork.local"
+TARGET="inspectionscope"
 class SerialInterface(threading.Thread):
     
     def __init__(self, port="/dev/ttyUSB0", baud=115200):
@@ -23,25 +25,25 @@ class SerialInterface(threading.Thread):
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.loop_start()
-        self.client.connect("gork.local")
+        self.client.connect(MQTT_SERVER)
 
     def on_connect(self, client, userdata, flags, rc):
-        self.client.subscribe("grblesp32/command")
-        self.client.subscribe("grblesp32/reset")
-        self.client.subscribe("grblesp32/cancel")
+        self.client.subscribe(f"{TARGET}/command")
+        self.client.subscribe(f"{TARGET}/reset")
+        self.client.subscribe(f"{TARGET}/cancel")
 
     def on_message(self, client, userdata, message):
         print(client, userdata, message)
-        if message.topic == 'grblesp32/command':
-            command = message.payload.decode('utf-8')
+        if message.topic == f"{TARGET}/command":
+            command = message.payload.decode("utf-8")
             print("Got command: ", command)
             self.write(command + "\n")
-        elif message.topic == 'grblesp32/reset':
-            if message.payload.decode('utf-8') == 'hard':
+        elif message.topic == f"{TARGET}/reset":
+            if message.payload.decode("utf-8") == "hard":
                 self.reset()
             else:
                 self.soft_reset()
-        elif message.topic == 'grblesp32/cancel':
+        elif message.topic == f"{TARGET}/cancel":
             print("cancel")
             self.serialport.write(bytes([0x85]))
             
@@ -59,15 +61,15 @@ class SerialInterface(threading.Thread):
         try:
             self.serialport.open()
         except serial.SerialException as e:
-            sys.stderr.write('Could not open serial port {}: {}\n'.format(ser.name, e))
+            sys.stderr.write("Could not open serial port {}: {}\n".format(ser.name, e))
             return
         self.reset()
         
         while True:
             if self.serialport.in_waiting > 0:
                 data = self.serialport.read(self.serialport.in_waiting)
-                sys.stdout.write(data.decode('utf-8'))
-                self.client.publish("grblesp32/output", data)
+                sys.stdout.write(data.decode("utf-8"))
+                self.client.publish(f"{TARGET}/output", data)
             time.sleep(0.01)
 
     def write(self, data):
@@ -80,5 +82,5 @@ def main():
     s.start()
     s.join()    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
